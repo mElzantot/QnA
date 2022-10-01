128014e71dedee198fc5a76f400c9cbd88a9f28e
Mohamed Shaker <m.elzantot@gmail.com>
2022-10-02 00:11:51 +0200

    add iquestion repo to override Get and delete methods from base Repo

---
+++ Qna.DAL/Repos/QuestionRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Qna.DAL.DBContext;
+using Qna.DAL.Generic;
+using Qna.DAL.Repos_Interfaces;
+using QnA.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qna.DAL.Repos
+{
+    public class QuestionRepository : Repository<Question>, IQuestionRepository
+    {
+        public QuestionRepository(AppDbContext appdbcontext) : base(appdbcontext)
+        {
+        }
+
+
+        public override async Task<Question?> GetByIdAsync(params object[] id)
+        {
+            return await entities.Include(q => q.Answers).Where(q => q.Id == (int)id[0])
+                                 .FirstOrDefaultAsync();
+        }
+
+        public override async Task<ICollection<Question>> GetAllAsync(Expression<Func<Question, bool>> predicate)
+
+        {
+            return await entities.Include(q => q.Answers).Where(predicate).OrderByDescending(q => q.Id)
+                    .ToListAsync();
+        }
+
+        public override async Task<bool> RemoveByIdAsync(params object[] id)
+        {
+            Question? question = await entities.FindAsync(id);
+            if (question == null) return false;
+            question.IsDeleted = true;
+            question.ModificationDate = DateTime.Now;
+            entities.Update(question);
+            return await context.SaveChangesAsync() > 0;
+        }
+    }
+}
