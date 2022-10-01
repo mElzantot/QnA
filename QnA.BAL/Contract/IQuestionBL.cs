fcb84a3281378c6566a175bcf68ba789870b9f57
Mohamed Shaker <m.elzantot@gmail.com>
2022-10-02 00:15:32 +0200

    add IQuestionBL

--- QnA.BAL/Contract/IQuestionBL.cs
+++ QnA.BAL/Contract/IQuestionBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QnA.BAL.DTO;
+using QnA.DbModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace QnA.BAL.Contract
 {
     public interface IQuestionBL
     {
-        
+        Task<QuestionProfile> AddNewQuestion(string questionBody);
+        Task<IEnumerable<Question>> GetAllQuestionsAsync();
+        Task<Question> GetQuestionWithAnswersAsync(int id);
+        Task<bool> DeleteQuestion(int id);
+
     }
 }
